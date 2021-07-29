219b1e9fdb41a635c49981ba341d70aec513b78e
Tran Quang Vinh <qvr@elca.vn>
2021-07-29 22:29:40 +0700

    Update code base with Junit, Mockito

--- Java/src/test/java/vn/elca/training/unittesting/service/impl/BankServiceTest.java
+++ Java/src/test/java/vn/elca/training/unittesting/service/impl/BankServiceTest.java
@@ -16,26 +16,25 @@
 
 package vn.elca.training.unittesting.service.impl;
 
-import java.util.Date;
-
-import org.testng.Assert;
-import org.testng.annotations.Test;
-
+import org.junit.jupiter.api.Test;
 import vn.elca.training.unittesting.dom.Person;
 import vn.elca.training.unittesting.service.IBankService;
 
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 /**
- * @author mmn
+ * @author qvr
  *
  */
-public class BankServiceTest {
+class BankServiceTest {
 
     /**
      * Method BankServiceImpl.calculateInterestRevenue computes the interest revenue based on the saving account of
      * person. This test case is used to test the correctness of the method.
      */
     @Test
-    public void calculateInterestRevenueWithDifferenceSavingAccountsShouldCorrectly() {
+    void testCalculateInterestRevenueWithDifferenceSavingAccountsShouldCorrectly() {
         // 1. Build input data.
         Person person = new Person(1, "Man", "Nguyen", new Date(), 3000, 2000);
 
@@ -47,6 +46,6 @@ public class BankServiceTest {
         double interestRevenue = bankService.calculateInterestRevenue(person);
 
         // 4. Check the result.
-        Assert.assertEquals(interestRevenueExpected, interestRevenue, "Interest revenue is wrongly calculated");
+        assertEquals(interestRevenueExpected, interestRevenue, "Interest revenue is wrongly calculated");
     }
 }
--- Java/src/test/java/vn/elca/training/unittesting/service/impl/TaxServiceTest.java
+++ Java/src/test/java/vn/elca/training/unittesting/service/impl/TaxServiceTest.java
@@ -16,38 +16,31 @@
 
 package vn.elca.training.unittesting.service.impl;
 
-import static org.easymock.EasyMock.createMock;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.expectLastCall;
-import static org.easymock.EasyMock.replay;
-import static org.easymock.EasyMock.verify;
-
 import java.util.Date;
-
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockito.Mockito;
 import vn.elca.training.unittesting.dom.Person;
-import vn.elca.training.unittesting.service.IBankService;
 import vn.elca.training.unittesting.service.IPersonTaxService;
 
 /**
- * @author mmn
+ * @author qvr
  *
  */
-public class TaxServiceTest {
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class TaxServiceTest {
 
-    private IBankService bankService = null;
     private TaxService taxService = null;
 
-    @BeforeClass
-    public void setup() {
+    @BeforeAll
+    void setup() {
         taxService = new TaxService();
     }
 
     @Test
-    public void testUpdateAnnualTax() {
+    void testUpdateAnnualTax() {
         // 1. Arrange.
         Person person = new Person(1, "Hung", "Nguyen", new Date(), 1000);
         person.setInheritTax(400);
@@ -58,24 +51,21 @@ public class TaxServiceTest {
         // If any call to the fake service personalTaxService
         // to calculate tax for the given person, just return dummy 'annualTax'
         // Note: The interface IPersonTaxService has no implementation.
-        IPersonTaxService personTaxServiceMock = createMock(IPersonTaxService.class);
-
+        IPersonTaxService personTaxServiceMock = Mockito.mock(IPersonTaxService.class);
 
         // 1.2 Record mock object behavior
         // a. Expect behaviour of method calculateAnnualTax of specified person to return value of variable annualTax
-        expect(personTaxServiceMock.calculateAnnualTax(person)).andReturn(mockAnnualTax);
+        Mockito.when(personTaxServiceMock.calculateAnnualTax(person)).thenReturn(mockAnnualTax);
 
         // b. Expect behaviour of method consolidateTax of specified person to behave as below.
-        personTaxServiceMock.consolidateTax(person);
-        expectLastCall().once();
+        Mockito.doNothing().when(personTaxServiceMock).consolidateTax(person);
 
         // 2. Act
-        replay(personTaxServiceMock);
         taxService.setPersonTaxService(personTaxServiceMock);
         Person updatedPerson = taxService.updateAnnualTax(person);
 
         // 3. Assert.
-        verify(personTaxServiceMock);
-        Assert.assertEquals(mockAnnualTax, updatedPerson.getAnnualTax(), "Incorrect Annual Tax");
+        //Mockito.verify(personTaxServiceMock);
+        Assertions.assertEquals(mockAnnualTax, updatedPerson.getAnnualTax(), "Incorrect Annual Tax");
     }
 }
